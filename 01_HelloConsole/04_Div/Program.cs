5a809f5daa8a0419908e58155f0eb0be4538f77a
x-fresh <danil_r99@mail.ru>
2023-09-18 17:30:08 +0300

    add 03_Sum

--- 01_HelloConsole/04_Div/Program.cs
+++ 01_HelloConsole/04_Div/Program.cs
@@ -1,3 +1,3 @@
-﻿double numberA = 12;
-double numberB = 5;
+﻿double numberA = new Random().Next(1, 10); // 1, 2, 3, 4, ... 9
+double numberB = new Random().Next(1, 10);
 System.Console.WriteLine(numberA / numberB);
