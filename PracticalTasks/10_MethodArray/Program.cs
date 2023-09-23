9e04e34808ac9abf1ae0e7a47d3da284914abba0
x-fresh <danil_r99@mail.ru>
2023-09-23 08:44:00 +0300

    2

---
+++ PracticalTasks/10_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 11, 15, 22, 42, 4565, 11, 45, 55 };
+
+int n = array.Length;
+int find = 11;
+ 
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //завершает работу алгоритма
+    }
+    //index = index + 1;
+    index++;   
+}
