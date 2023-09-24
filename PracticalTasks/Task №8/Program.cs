66fd1968458ceab3b166d47843a50f652bba3271
x-fresh <danil_r99@mail.ru>
2023-09-24 19:06:48 +0300

    add DZ1

--- PracticalTasks/Task №8/Program.cs
+++ PracticalTasks/Task №8/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-Console.Write("Введите: ");
+Console.Write("Введите N: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 int evenNumber = 2;
