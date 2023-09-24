629fa0fa34f2d587834fcb00eb76a712447745e8
x-fresh <danil_r99@mail.ru>
2023-09-24 18:39:04 +0300

    5

--- PracticalTasks/Seminar_01/Program.cs
+++ PracticalTasks/Seminar_01/Program.cs
@@ -4,7 +4,7 @@ System.Console.Write("imput N: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 int count = -n;
-while (count <=n)
+while (count <= n)
 {
-    System.Console.Write(count++ + " ");
+    System.Console.WriteLine(count++ + " ");  
 }
---
+++ PracticalTasks/Task №6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("Число четное ");
+}
+else
+{
+    Console.WriteLine("Число нечетное ");
+}
+
+
+
