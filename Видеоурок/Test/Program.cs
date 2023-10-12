88d78747146164bb6bc1517ed6b77d9bea6da0c9
x-fresh <danil_r99@mail.ru>
2023-10-12 18:44:28 +0300

    add

---
+++ Видеоурок/Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Test
+{
+    class Program 
+    {
+        float user_inprint;      // (данные от пользователя)
+        {
+            user_inprint = float.Parse(Console.ReadLine());
+            
+            
+        }
+
+        // user_inprint = Convert.ToDouble(Console.ReadLine()); // (конвертация данных от пользователя в тип данных double)
+        // Если в конвертации отсутствует тип данных к оторый хоть конвертировать, то конвертация невозможно
+        // Например ToFloat отсутствует.
+
+
+
+
+    }
+}
---
+++ Видеоурок/Конспект/Program.cs
@@ -0,0 +1,26 @@
+﻿        Типы базданных:
+        // short number = 33000;
+           // byte = 256;
+           // long = 231232323323 очень больше числа
+           // float num = 1.43434f 
+           // duble num_2 = 1.3232332d (диапазон больше float в 2 раза, (всегда ставиться точка))
+        //    int number = 10;
+        //    string word = "Hello ..."; // Позваляет хранить больше кол-во символов
+        //    char symbol = 's'; // Позвалять хранить только один символ (использует одинарные скобки)
+            // bool isHappy = true, false;
+
+
+        Конвертация:
+                    // static void Main()
+        // {
+        //     int num_1 = 0, num_2 = 0;
+
+        //     num_1 = Convert.ToInt32(Console.ReadLine()); Convert (конвертировать) ToInt32 (тип данных интенжер 32 бита (int))
+        //     num_2 = Convert.ToInt32(Console.ReadLine());
+            
+        //     Console.WriteLine("first " + num_1 + " " + "second " + num_2);
+        // }
+        
+        // user_inprint = Convert.ToDouble(Console.ReadLine()); // (конвертация данных от пользователя в тип данных double)
+        // Если в конвертации отсутствует тип данных к оторый хоть конвертировать, то конвертация невозможно
+        // Например ToFloat отсутствует.
